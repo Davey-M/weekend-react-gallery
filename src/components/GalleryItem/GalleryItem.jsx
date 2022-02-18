e0c1864e34909cfd1bf884f7f27b34446a80aea5
Davey-M <davey.meuer@gmail.com>
2022-02-18 16:41:11 -0600

    add description toggle

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -2,15 +2,30 @@ import { useState } from "react";
 
 function GalleryItem({ image, updateImage }) {
 
+    const [ description, toggleDescription ] = useState(false);
+
     const handleClickLike = () => {
         updateImage(image.id);
     }
 
+    const handleDescriptionToggle = () => {
+        toggleDescription(!description);
+    }
+
+    const imageStyles = {
+        width: 'max-content',
+        margin: 'auto',
+    }
+
     return (
         <>
-            <div>
-                <img src={ image.path } alt="" width="200" />
-                <p>{ image.description }</p>
+            <div onClick={ handleDescriptionToggle } style={imageStyles}>
+                <img 
+                    src={ image.path }
+                    alt="Image Not Found"
+                    width="200"
+                />
+                {description && <p>{ image.description }</p>}
             </div>
             <div>
                 <p>{ image.likes } likes</p>
