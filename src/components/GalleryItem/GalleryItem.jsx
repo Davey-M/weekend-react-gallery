cb4e213255c3bb33985c6d643b22b1d32e0c8b3b
Davey-M <davey.meuer@gmail.com>
2022-02-18 16:47:51 -0600

    add description styling

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -15,6 +15,18 @@ function GalleryItem({ image, updateImage }) {
     const imageStyles = {
         width: 'max-content',
         margin: 'auto',
+        position: 'relative',
+    }
+
+    const descriptionStyles = {
+        width: '100%',
+        height: '100%',
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        backgroundColor: '#000000aa',
+        margin: '0rem',
+        color: '#ffffff',
     }
 
     return (
@@ -25,7 +37,7 @@ function GalleryItem({ image, updateImage }) {
                     alt="Image Not Found"
                     width="200"
                 />
-                {description && <p>{ image.description }</p>}
+                {description && <p style={descriptionStyles}>{ image.description }</p>}
             </div>
             <div>
                 <p>{ image.likes } likes</p>
