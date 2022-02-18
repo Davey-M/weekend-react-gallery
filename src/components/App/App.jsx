a2d7f3bf88272d050b2c3eda978b63a98019386e
Davey-M <davey.meuer@gmail.com>
2022-02-18 16:09:02 -0600

    start getItems

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+const { useState, useEffect } = React;
+import Axios from 'axios';
 import './App.css';
 
 function App() {
+
+    const [ listItems, setListItems ] = useState([]);
+
+    const getItems = () => {
+        Axios.get('/gallery')
+            .then(response => {
+                setListItems(response);
+            })
+            .catch(err => {
+                console.error('Error in getItems:', err);
+            })
+    }
+
+    useEffect(() => {
+        getItems();
+    }, []);
+
     return (
         <div className="App">
             <header className="App-header">
