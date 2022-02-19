06a4052b67d9133231368fca05b7d9bdbd13530b
Davey-M <davey.meuer@gmail.com>
2022-02-19 11:40:04 -0600

    add comments

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,5 +1,6 @@
 const pg = require('pg');
 
+// database connection options
 const options = {
     host: 'localhost',
     database: 'react-gallery',
@@ -8,14 +9,18 @@ const options = {
     idleTimeoutMillis: 30_000,
 }
 
+// db connection object
 const pool = new pg.Pool(options);
 
+// send success status when db is connected
 pool.on('connect', () => {
     console.log(`Connected to database ${options.database} at ${Date.now()}`);
 })
 
+// send error status when there is a db error
 pool.on('error', (err) => {
     console.error(`Error with database ${options.database} at ${Date.now()}: ${err}`);
 })
 
+// export pool
 module.exports = pool;
